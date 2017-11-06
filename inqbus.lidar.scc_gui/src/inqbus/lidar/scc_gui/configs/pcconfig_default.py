46aca17a5e835d7a69522744bd0eb70053254065
sandrarum <sandra.rum@inqbus.de>
2017-11-06 16:32:10 +0100

    Redtored default configs after merge

--- inqbus.lidar.scc_gui/src/inqbus/lidar/scc_gui/configs/pcconfig_default.py
+++ inqbus.lidar.scc_gui/src/inqbus/lidar/scc_gui/configs/pcconfig_default.py
@@ -1,25 +1,13 @@
 import os
 
-# this path can be your general base path. It is not mandatory.
-# You may also provide the following paths directly,
-# not in relation to this base path
-BASE_PATH = '/'
+BASE_PATH = 'C:\\inqbus.lidar\\test_data'
 
-# This is the directory, where your raw lidar data are located
 DATA_PATH = os.path.join(BASE_PATH, 'data')
+TEMP_PATH = 'c:\\temp'
 
-# If your raw lidar data are zip files, the are temporarily unpacked into this directory.
-TEMP_PATH = os.path.join(BASE_PATH, 'temp')
+OUT_PATH = os.path.join(BASE_PATH, 'scc_raw')
+SONDE_PATH = os.path.join(BASE_PATH, 'sondes')
+OVL_PATH = os.path.join(BASE_PATH, 'overlaps')
 
-# the created scc raw data files and the corresponding ancillary files are written into this directory
-OUT_PATH = os.path.join(DATA_PATH, 'scc_raw')
-
-# This is the directory, where radio sonde files are located.
-SONDE_PATH = os.path.join(DATA_PATH, 'sondes')
-
-# this is the directory, where overlap files are located
-# not yet implemented
-#OVL_PATH = os.path.join(DATA_PATH, 'overlaps')
-
-# log files are written into this directory
+IN_PATH = os.path.join(BASE_PATH, 'raw_nc')
 LOG_PATH = os.path.join(BASE_PATH, 'logs')
