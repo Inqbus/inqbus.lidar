c720d74f39c726cb1dac24e1b96d70a5fe363f3b
sandra <sandra.rum@inqbus.de>
2017-11-09 17:27:06 +0100

    Replaced manual axisthicks by overwriting setRange. So axis gets a range which already uses m or km.

--- inqbus.lidar.scc_gui/src/inqbus/lidar/scc_gui/axis.py
+++ inqbus.lidar.scc_gui/src/inqbus/lidar/scc_gui/axis.py
@@ -24,46 +24,81 @@ class HeightAxis(DataAxis):
     """
     axis_data = None
 
-    def tickStrings(self, values, scale, spacing):
-        strns = []
-        datavalues = []
-        for value in values:
-            if value < self.axis_data.shape[0]:
-                datavalues.append(self.axis_data[int(value)])
-        try:
-            min_value = min(datavalues)
-        except BaseException:
-            logger.error("Exception: %s" % sys.exc_info()[0])
-            logger.error("Traceback: %s" % tb.format_exc())
-            min_value = self.axis_data[0]
-        try:
-            max_value = max(datavalues)
-        except BaseException:
-            logger.error("Exception: %s" % sys.exc_info()[0])
-            logger.error("Traceback: %s" % tb.format_exc())
-            max_value = self.axis_data[-1]
+    # def tickStrings(self, values, scale, spacing):
+    #     strns = []
+    #     datavalues = []
+    #     for value in values:
+    #         if value < self.axis_data.shape[0]:
+    #             datavalues.append(self.axis_data[int(value)])
+    #     try:
+    #         min_value = min(datavalues)
+    #     except BaseException:
+    #         logger.error("Exception: %s" % sys.exc_info()[0])
+    #         logger.error("Traceback: %s" % tb.format_exc())
+    #         min_value = self.axis_data[0]
+    #     try:
+    #         max_value = max(datavalues)
+    #     except BaseException:
+    #         logger.error("Exception: %s" % sys.exc_info()[0])
+    #         logger.error("Traceback: %s" % tb.format_exc())
+    #         max_value = self.axis_data[-1]
+    #
+    #     rng = abs(max_value - min_value)
+    #
+    #     if rng < KILOMETER_LABEL_RANGE:
+    #         for x in datavalues:
+    #             try:
+    #                 strns.append(str(round(x, 3)))
+    #             except BaseException:
+    #                 strns.append('')
+    #         label = 'm'
+    #     else:
+    #         # TODO: Sometimes single values are displayed as meter
+    #         for x in datavalues:
+    #             try:
+    #                 strns.append(str(round(x / 1000., 2)))
+    #             except BaseException:
+    #                 strns.append('')
+    #         label = 'km'
+    #
+    #     self.setLabel(text=label)
+    #     return strns
 
-        rng = abs(max_value - min_value)
+    def setRange(self, mn, mx):
+        if self.axis_data is None:
+            return super(HeightAxis, self).setRange(mn, mx)
+        size = self.axis_data.size
+        if int(mn) > size:
+            mn_new = self.calculate_over_end(int(mn))
+        else:
+            mn_new = self.axis_data[int(mn)]
 
-        if rng < KILOMETER_LABEL_RANGE:
-            for x in datavalues:
-                try:
-                    strns.append(str(round(x, 3)))
-                except BaseException:
-                    strns.append('')
+        if int(mx) > size:
+            mx_new = self.calculate_over_end(int(mx))
+        else:
+            mx_new = self.axis_data[int(mx)]
+
+        rng = abs(mx_new-mn_new)
+
+        if rng == 0.0:
+            return
+        elif rng < KILOMETER_LABEL_RANGE:
             label = 'm'
         else:
-            # TODO: Sometimes single values are displayed as meter
-            for x in datavalues:
-                try:
-                    strns.append(str(round(x / 1000., 2)))
-                except BaseException:
-                    strns.append('')
             label = 'km'
-
+            mx_new = mx_new / 1000.0
+            mn_new = mn_new / 1000.0
         self.setLabel(text=label)
-        return strns
+        return super(HeightAxis, self).setRange(mn_new, mx_new)
 
+    def calculate_over_end(self, mn):
+        size = self.axis_data.size
+        last_entry = self.axis_data[-1]
+        first_entry = self.axis_data[0]
+        distance = (last_entry-first_entry) / size
+        offset_index = mn - size
+        res = distance * offset_index + last_entry
+        return res
 
 class DateAxis(DataAxis):
     """
