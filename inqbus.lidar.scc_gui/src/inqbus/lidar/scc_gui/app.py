c6db577fba37dc0e0e7bee2fa2cfc11d8cd6a737
imattis <ina@ina-mattis.de>
2018-01-17 13:29:07 +0100

    corrected: create name of lidarlog file (YYYYMMDD_temps.txt)

--- inqbus.lidar.scc_gui/src/inqbus/lidar/scc_gui/app.py
+++ inqbus.lidar.scc_gui/src/inqbus/lidar/scc_gui/app.py
@@ -87,7 +87,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
     def newQuicklookPlot(self):
         file_path = self.showRawOpenDialog()
         file_name = os.path.basename(file_path)
-        log_file = os.path.join(mc.LIDAR_LOG_PATH, file_name[:8] + '_temps.txt')
+        log_file = os.path.join(mc.LIDAR_LOG_PATH, file_name.replace('_','')[:8] + '_temps.txt')
 
         if not os.path.exists(mc.LIDAR_LOG_PATH):
             logger.warning("%s can not be found. Check if paths are configured correctly and all directories exist." % mc.LIDAR_LOG_PATH)
